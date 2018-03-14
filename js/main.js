b517f2a4db269aaf74ac13c8c63f3c400e8178cc
Samir Muratovic <ssamirmuratovic@gmail.com>
2018-03-13 23:43:35 -0400

    add featured projects section in markup

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ var menuBtn = document.getElementById('menu-btn');
 
 
 menuBtn.addEventListener('click', menuBtnClicked);
+menuBtn.addEventListener('mouseover', changeSidebarBorder);
+menuBtn.addEventListener('mouseout', originalSidebarBorder);
+
+
+
+
 let counter = 0;
 
 function menuBtnClicked(e) {
@@ -24,4 +30,12 @@ function closeSidebarMenu() {
   sidebar.style.transform = 'translateX(0px)';
   sidebar.style.width = '50px';
   sidebar.style.backgroundColor = 'inherit';
+}
+
+function changeSidebarBorder(e) {
+  sidebar.style.borderLeft = '6px solid #ddd';
+}
+
+function originalSidebarBorder(e) {
+  sidebar.style.borderLeft = '6px solid #a4fbe7';
 }
