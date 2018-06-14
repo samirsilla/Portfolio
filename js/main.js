41b317524a4beac56ba136109d404c89302e7aa1
Samir Muratovic <ssamirmuratovic@gmail.com>
2018-06-14 19:28:54 -0400

    refactor main.js

--- js/main.js
+++ js/main.js
@@ -1,47 +1,46 @@
-var sidebar = document.getElementById('sidebar');
-var menuBtn = document.getElementById('menu-btn');
-var navMenu = document.getElementById('menu');
-var aboutLink = document.getElementById('about-link');
-var portfolioLink = document.getElementById('portfolio-link');
-var closeIcon = document.getElementById('close-icon');
+const sidebar = document.getElementById('sidebar');
+const menuBtn = document.getElementById('menu-btn');
+const navMenu = document.getElementById('menu');
+const aboutLink = document.getElementById('about-link');
+const portfolioLink = document.getElementById('portfolio-link');
+const closeIcon = document.getElementById('close-icon');
 
-menuBtn.addEventListener('click', menuBtnClicked);
-menuBtn.addEventListener('mouseover', changeSidebarBorder);
-menuBtn.addEventListener('mouseout', originalSidebarBorder);
+menuBtn.addEventListener('click', isMenuOpen);
+menuBtn.addEventListener('mouseover', setMenuColorToHoverState);
+menuBtn.addEventListener('mouseout', setMenuColorToOriginalState);
+aboutLink.addEventListener('click', isMenuOpen);
+portfolioLink.addEventListener('click', isMenuOpen);
+closeIcon.addEventListener('click', isMenuOpen);
 
-aboutLink.addEventListener('click', menuBtnClicked);
-portfolioLink.addEventListener('click', menuBtnClicked);
-closeIcon.addEventListener('click', menuBtnClicked);
+let menuOpen = false;
 
-let counter = 0;
-
-function menuBtnClicked(e) {
-  if (counter % 2 == 0) {
-    openSidebarMenu();
+function isMenuOpen(e) {
+  if (!menuOpen) {
+    openMenu();
   } else {
-    closeSidebarMenu();
+    closeMenu();
   }
-  counter++;
+  menuOpen = !menuOpen;
 }
 
-function openSidebarMenu() {
+function openMenu() {
   sidebar.style.transform = 'translateX(80vw)';
   sidebar.style.width = '20vw';
   sidebar.style.backgroundColor = 'rgba(180, 180, 180, 0.6)';
   navMenu.style.transform = "translateX(80vw)";
 }
 
-function closeSidebarMenu() {
+function closeMenu() {
   navMenu.style.transform = 'translateX(-20vw)';
   sidebar.style.transform = 'translateX(0vw)';
   sidebar.style.width = '50px';
   sidebar.style.backgroundColor = 'inherit';
 }
 
-function changeSidebarBorder(e) {
+function setMenuColorToHoverState(e) {
   sidebar.style.borderLeft = '6px solid #ddd';
 }
 
-function originalSidebarBorder(e) {
+function setMenuColorToOriginalState(e) {
   sidebar.style.borderLeft = '6px solid #55f7d4';
 }
